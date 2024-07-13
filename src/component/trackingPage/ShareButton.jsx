1bb2b7caa4b541990f91c518b12061eb2aac0256
Deepali <deepalisahu965@gmail.com>
2024-07-13 10:05:24 +0530

    change 3

--- src/component/trackingPage/ShareButton.jsx
+++ src/component/trackingPage/ShareButton.jsx
@@ -2,10 +2,12 @@ import React from "react";
 import { useLocation } from "react-router-dom";
 import "./ShareButton.css";
 import { auth } from "../firebase";
+import { useNavigate, Link } from "react-router-dom";
 
 const ShareButton = ({ speed }) => {
   const location = useLocation();
   const shareUrl = `${window.location.origin}${location.pathname}?speed=${speed}`;
+  const navigate = useNavigate();
 
   const handleShare = () => {
     navigator.clipboard.writeText(shareUrl);
@@ -15,7 +17,7 @@ const ShareButton = ({ speed }) => {
   async function handleLogout() {
     try {
       await auth.signOut();
-      window.location.href = "/login";
+      navigate("/login");
       console.log("User logged out successfully!");
     } catch (error) {
       console.error("Error logging out:", error.message);
